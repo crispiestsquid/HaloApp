6895b5e34e17c6a918d8dc89f2ff1608a604c63f
Kevin Little <crispiestsquid@aol.com>
2021-01-02 23:37:22 -0500

    Added Params to Route

--- backend/routes/spartanCompanyRouter.js
+++ backend/routes/spartanCompanyRouter.js
@@ -3,30 +3,29 @@ const axios = require('axios');
 const spartanCompanyUtils = require('./utils/companyInfo');
 const spartanCompanyRouter = express.Router();
 
-spartanCompanyRouter.route('/')
-.get(async (req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<body><h1>Hello World!</h1></body>');
-    try { 
-        const test = await spartanCompanyUtils.getCompanyInfo("CrankiestSeeker");
-        console.log(test);
-    } catch(Error) {
-        console.log(Error)
-    }
-
-});
+spartanCompanyRouter.route('/:gamertag')
+    .get(async (req, res, next) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        res.end('<body><h1>Hello World!</h1></body>');
+        try {
+            const test = await spartanCompanyUtils.getCompanyInfo(req.params.gamertag);
+            console.log(test);
+        } catch (Error) {
+            console.log(Error)
+        }
+    });
 
 spartanCompanyRouter.route('/:companyName')
-.get((req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end(`<body><h1>Hello ${req.params.companyName}!</h1></body>`);
-})
-.post((req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.json({companyName: req.params.companyName});
-});
+    .get((req, res, next) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        res.end(`<body><h1>Hello ${req.params.companyName}!</h1></body>`);
+    })
+    .post((req, res, next) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({ companyName: req.params.companyName });
+    });
 
 module.exports = spartanCompanyRouter;
--- backend/routes/utils/companyInfo.js
+++ backend/routes/utils/companyInfo.js
@@ -3,33 +3,33 @@ const halo_api = "https://www.haloapi.com"
 const api_key = process.env.HALO_API_KEY;
 
 axios.defaults.headers = {
-       'Content-Type': 'application/json',
-       'Ocp-Apim-Subscription-Key': api_key
-   }
+	'Content-Type': 'application/json',
+	'Ocp-Apim-Subscription-Key': api_key
+}
 
 const getCompanyInfo = async player => {
-    let data = await getCompany(player);
-    let company_id = data.Company.Id;
-    let response = await axios.get(`${halo_api}/stats/h5/companies/${company_id}`)
-	.then(res => {
-	    return res.data;
-	})
-	.catch( error => {
-	    return error;
-	})
-    return response;
+	let data = await getCompany(player);
+	let company_id = data.Company.Id;
+	let response = await axios.get(`${halo_api}/stats/h5/companies/${company_id}`)
+		.then(res => {
+			return res.data;
+		})
+		.catch(error => {
+			return error;
+		})
+	return response;
 }
 
 // Get Company Object based on a Gamertag
 const getCompany = async player => {
-    let response = await axios.get(`${halo_api}/profile/h5/profiles/${player}/appearance`)
-	.then(res => {
-	    return res.data;
-	})
-	.catch(error => {
-	    return error;
-	})
-    return response;
+	let response = await axios.get(`${halo_api}/profile/h5/profiles/${player}/appearance`)
+		.then(res => {
+			return res.data;
+		})
+		.catch(error => {
+			return error;
+		})
+	return response;
 }
 
-module.exports = { getCompany,getCompanyInfo }
+module.exports = { getCompany, getCompanyInfo }
