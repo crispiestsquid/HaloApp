682fa00377b68e1a36ccdf1615ac2ee9a9ba0794
Shas <dmerryman@smcm.edu>
2021-01-10 16:30:02 +0000

    Added dictionary for medals that count towards commendations

--- backend/routes/utils/statics/achilles_commendations.js
+++ backend/routes/utils/statics/achilles_commendations.js
@@ -32,4 +32,69 @@ let achillesCommendations = [
 	"Deconstructed",
 ];
 
-module.exports = achillesCommendations;
+let achillesMedals = {
+	2287626681:	"Sting like a bee", 		// Melee Kill
+	1219497744: 	"Grand Theft", 			// Hijack
+	1801925525: 	"Grand Theft", 			// Skyjack
+	685960231: 	"Kicking it Old School", 	// BXR
+	2683910456: 	"Kicking it Old School", 	// Noob combo
+	1618319591: 	"Kicking it Old School", 	// Nadeshot
+	285057226: 	"The Reaper", 			// From the Grave
+	3400287617: 	"Almost doesn't count", 	// Close Call
+	2531822079: 	"Lucky", 			// Cluster Luck
+	2494364276: 	"Lucky", 			// Last Shot
+	3601671677: 	"Lucky", 			// Sayonara
+	3786961025: 	"Lucky", 			// Fastball
+	4164420745: 	"Not so tough", 		// Boss Takedown
+	1524161777: 	"Not so tough", 		// Legendary Boss Takedown
+	3703205413: 	"Not so tough", 		// Mythic Boss Takedown
+	3261908037: 	"Something on your face", 	// Headshot
+	2782465081: 	"Too fast for you",		// Reversal
+	2763748638: 	"Is there no one else?",	// Triple Kill
+	1080468863: 	"I'm just perfect",		// Perfect Kill - BR
+	3992195104: 	"I'm just perfect",		// Perfect Kill - H1 Magnum
+	3098362934: 	"I'm just perfect",		// Perfect Kill - Carbine
+	2279899989: 	"I'm just perfect",		// Perfect Kill - LightRifle
+	370413844: 	"I'm just perfect",		// Perfect Kill - DMR
+	3653057799: 	"I'm just perfect",		// Perfect Kill - Magnum
+	4204396686: 	"Forgot to pay the toll",	// Buckle Up
+	//        :	"Standard Issue",		Standard Issue is different - requires tracking commendations, not medals
+	//        :	"It's a bird!",			It's a bird! is different - requires tracking commendations, not medals
+	2093481574: 	"Till someone loses an eye",	// Snipeltaneous
+	3486286344: 	"Till someone loses an eye",	// Airborne Snapshot
+	1986137636: 	"Till someone loses an eye",	// Snapshot
+	848240062: 	"Till someone loses an eye",	// Sniper Headshot
+	3676723563: 	"Lawnmower",			// Splatter 
+	//	  : 	"No Hard Feelings",		No Hard Feelings is different - Tracked by spartan kills 
+	3344421840: 	"Look ma no pin",		// Grenade Kill 
+	298813630: 	"The Pain Train",		// Spartan Charge 
+	//	  : 	"Road Trip",			Road Trip is different - Tracked by commendations, not medals
+	//	  : 	"Power play",			Power player is different - Tracked by power weapon kills
+	492192256: 	"From the top rope",		// Ground Pound 
+	2966496172: 	"So cuddly",			// Assassination 
+	3001183151:	"First Strike",			// First Strike
+	1584076385:	"Sorry mate",			// Marine kill
+	515648662:	"Vandalism",			// Mongoose Destroyed
+	2497544753:	"Vandalism",			// Ghost Destroyed
+	92444561:	"Vandalism",			// Warthog Destroyed
+	762229696:	"Vandalism",			// Banshee Destroyed
+	615190505:	"Vandalism",			// Mantis Destroyed
+	1568252876:	"Vandalism",			// Wraith Destroyed
+	35545941:	"Vandalism",			// Scorpion Destroyed
+	1795642208:	"Vandalism",			// Phaeton Destroyed
+	1711392399:	"Vandalism",			// Wasp Destroyed
+	3491849182:	"From Downtown",		// Hail Mary
+	1423504140:	"From Downtown",		// Longshot
+	882267095:	"From Downtown",		// Spray N' Pray
+	281002471:	"Not so fast",			// Busted
+	2838259753:	"Body Guard",			// Protector
+	2955425834:	"Can't we get along",		// Elite Kill
+	3324603383:	"Can't we get along",		// Grunt Kill
+	1862589993:	"Can't we get along",		// Jackal Kill
+	519459233:	"Deconstructed",		// Watcher Kill
+	2947060439:	"Deconstructed",		// Crawler Kill
+	1311847356:	"Deconstructed",		// Knight Kill
+	3344008582:	"Deconstructed",		// Soldier Kill
+};
+
+module.exports = { achillesCommendations, achillesMedals };
