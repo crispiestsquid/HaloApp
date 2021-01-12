6228e326e5a1e03534740076c1b8cb2648ce291d
Shas <dmerryman@smcm.edu>
2021-01-12 00:08:25 +0000

    fixed endpoint; added spartan commendations

--- backend/routes/utils/metadata.js
+++ backend/routes/utils/metadata.js
@@ -6,7 +6,7 @@ const getCustomMetadata = async () => {
 	// TODO: Placeholder for custom metadata we may add
 };
 
-// Return Formatted JSON object from Halo API with Commendation names as keys
+// Return formatted JSON object from Halo API with Commendation names as keys
 // Ex:     "4 Little Spartans went out to play": {
 //    		    "category": {
 //    		        "contentId": "1e319878-93c1-41b0-b7bc-8219fe1e319f",
@@ -21,7 +21,7 @@ const getCustomMetadata = async () => {
 //    		    "id": "6eb5140f-7243-4b5d-91da-1b24dbdc3324",
 //    		    ...
 const getCompCommMeta = async () => {
-	let response = await axios.get(`${halo_api}/metadata/h5/metadata/campaign-missions`)
+	let response = await axios.get(`${halo_api}/metadata/h5/metadata/company-commendations`)
 	.then (res => {
 		return res.data;
 	})
@@ -32,6 +32,41 @@ const getCompCommMeta = async () => {
 	response.forEach( (commendation) => {
 		formatted_dict[commendation.name] = commendation;
 	})
+	return formatted_dict;
+}
+
+// Return formatted JSON object from Halo API with id as keys
+// Ex:	   "6fd87bf0-a3c8-47f6-af0a-6523fafdf75d": {
+//	        "type": "Progressive",
+//	        "name": "Phaeton Gunship Gunner ",
+//	        "description": "Kill enemy Spartans with a Phaeton Gunship",
+//	        "iconImageUrl": "https://content.halocdn.com/media/Default/games/halo-5-guardians/commendation-icons/commendations_array098-937bc98009384a34b097573b4b80f509.png",
+//	        "levels": [
+//	            {
+//	                "reward": {
+//	                    "xp": 100,
+//	                    "requisitionPacks": [],
+//	                    "id": "be0d2a2d-ee4e-4554-8894-8d23ded1e4a3",
+//	                    "contentId": "be0d2a2d-ee4e-4554-8894-8d23ded1e4a3"
+//	                },
+//	                "threshold": 5,
+//	                "id": "052ba3ef-f9de-4233-8f86-499dd38a3a20",
+//	                "contentId": "052ba3ef-f9de-4233-8f86-499dd38a3a20"
+//	            },
+//		...
+const getSpartanCommMeta = async () => {
+	let response = await axios.get(`${halo_api}/metadata/h5/metadata/commendations`)
+	.then (res => {
+		return res.data;
+	})
+	.catch( error => {
+		return error;
+	})
+	let formatted_dict = {};
+	response.forEach( (commendation) => {
+		formatted_dict[commendation.id] = commendation;
+	})
+
 	return formatted_dict;
 }
 
